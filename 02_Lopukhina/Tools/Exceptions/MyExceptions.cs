36c3facb53bd7618855e3d0508a47fd58c9e5493
Serafima <sima14@i.ua>
2019-03-19 08:46:24 +0200

    added missed files

---
+++ 02_Lopukhina/Tools/Exceptions/MyExceptions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _02_Lopukhina.Tools.Exceptions
+{
+    class OldToBeAlive : Exception
+    {
+        public OldToBeAlive(int age) : base($"Hmm, you are too old to be alive!\n The oldest man on the Erath was 122, and you are {age}. \n If you are alive, than congratulation and I'm pressing F")
+        { }
+    }
+
+    class NotBorn : Exception
+    {
+        public NotBorn() : base("Hey, you haven't born yet!")
+        { }
+    }
+
+    class NotValidEmail : Exception
+    {
+        public NotValidEmail(string email) : base($"You must have missed something in your email address {email}. \n Check it one more time!")
+        { }
+    }
+}
